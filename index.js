5a66acb8fc6a5d127fbab8e732689b23b316e7ac
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-01-19 23:55:31 -0700

    html generated from random username

--- index.js
+++ index.js
@@ -4,362 +4,105 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+var mColor;
+var mData;
+var mMapLocation;
+
 function promptUser() {
   return inquirer.prompt([
     {
       type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
+      name: "userName",
+      message: "Enter your GitHub Username: "
     },
     {
       type: "input",
-      name: "color",
-      message: "Enter a color (Green, Blue, Pink, or Red)"
+      name: "colorChoice",
+      message: "Enter a color (green, blue, pink, or red): "
     }
   ]);
 }
 
-function generateHTML(data) {
-  return `<!DOCTYPE html>
+function gitData(answers){
+   var urlGit = 'https://api.github.com/users/' + answers.userName;
+
+   const request = require('request');
+
+   const options = {
+       url: urlGit,
+       method: 'GET',
+       headers: {
+           'Accept': 'application/json',
+           'Accept-Charset': 'utf-8',
+           'User-Agent': 'request'
+       }
+   };
+
+   request(options, (error, response, body) => {
+     if (!error && response.statusCode == 200) {
+        mData = JSON.parse(body);
+        mMapLocation = "http://www.google.com/maps/place/" + mData.location;
+        const html = generateHTML(mData);
+        return writeFileAsync("index.html", html);
+     }
+   });
+}
+
+function generateHTML(answers) {
+  return `
+<!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-      <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-      <title>Document</title>
-      <style>
-          @page {
-            margin: 0;
-          }
-         *,
-         *::after,
-         *::before {
-         box-sizing: border-box;
-         }
-         html, body {
-         padding: 0;
-         margin: 0;
-         }
-         html, body, .wrapper {
-         height: 100%;
-         }
-         .wrapper {
-         background-color: ${colors[data.color].wrapperBackground};
-         padding-top: 100px;
-         }
-         body {
-         background-color: white;
-         -webkit-print-color-adjust: exact !important;
-         font-family: 'Cabin', sans-serif;
-         }
-         main {
-         background-color: #E9EDEE;
-         height: auto;
-         padding-top: 30px;
-         }
-         h1, h2, h3, h4, h5, h6 {
-         font-family: 'BioRhyme', serif;
-         margin: 0;
-         }
-         h1 {
-         font-size: 3em;
-         }
-         h2 {
-         font-size: 2.5em;
-         }
-         h3 {
-         font-size: 2em;
-         }
-         h4 {
-         font-size: 1.5em;
-         }
-         h5 {
-         font-size: 1.3em;
-         }
-         h6 {
-         font-size: 1.2em;
-         }
-         .photo-header {
-         position: relative;
-         margin: 0 auto;
-         margin-bottom: -50px;
-         display: flex;
-         justify-content: center;
-         flex-wrap: wrap;
-         background-color: ${colors[data.color].headerBackground};
-         color: ${colors[data.color].headerColor};
-         padding: 10px;
-         width: 95%;
-         border-radius: 6px;
-         }
-         .photo-header img {
-         width: 250px;
-         height: 250px;
-         border-radius: 50%;
-         object-fit: cover;
-         margin-top: -75px;
-         border: 6px solid ${colors[data.color].photoBorderColor};
-         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-         }
-         .photo-header h1, .photo-header h2 {
-         width: 100%;
-         text-align: center;
-         }
-         .photo-header h1 {
-         margin-top: 10px;
-         }
-         .links-nav {
-         width: 100%;
-         text-align: center;
-         padding: 20px 0;
-         font-size: 1.1em;
-         }
-         .nav-link {
-         display: inline-block;
-         margin: 5px 10px;
-         }
-         .workExp-date {
-         font-style: italic;
-         font-size: .7em;
-         text-align: right;
-         margin-top: 10px;
-         }
-         .container {
-         padding: 50px;
-         padding-left: 100px;
-         padding-right: 100px;
-         }
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Generator</title>
+</head>
+</body>
+<body>
+    <header>
+        <div class="wrapper">
+        <img src=${answers.avatar_url} alt="Profile Image" style="width:150px;height:150px;">
+        </div>
+        <h1>Hi!</h1>
+        <h2>My name is ${answers.login}!</h2>
+        <h5>Currently @ ${answers.company}</h5>
+        <p><a href=${mMapLocation}>${answers.location}</a></p>
+        <p><a href=${answers.html_url}>GitHub</a></p>
+        <p><a href=${answers.blog}>Blog</a></p>
+    </header>
+    <main>
+        <h3>${answers.bio}</h3>
+        <div class="card">
+            <h3>Public Repositories</h3>
+            <h4>${answers.public_repos}</h4>
+        </div>
+        <div class="card">
+                <h3>Followers</h3>
+                <h4>${answers.followers}</h4>
+        </div>
+        <div class="card">
+                <h3>GitHub Stars</h3>
+                <h4>${answers.public_gists}</h4>
+        </div>
+        <div class="card">
+                <h3>Following</h3>
+                <h4>${answers.following}</h4>
+        </div>
+    </main>
+</body>
+</html>`;
+}
 
-         .row {
-           display: flex;
-           flex-wrap: wrap;
-           justify-content: space-between;
-           margin-top: 20px;
-           margin-bottom: 20px;
-         }
-
-         .card {
-           padding: 20px;
-           border-radius: 6px;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           margin: 20px;
-         }
-         
-         .col {
-         flex: 1;
-         text-align: center;
-         }
-
-         a, a:hover {
-         text-decoration: none;
-         color: inherit;
-         font-weight: bold;
-         }
-
-         @media print { 
-          body { 
-            zoom: .75; 
-          } 
-         }
-      </style>`
-        }
-
-
-// function generateHTML(answers) {
-//   return `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-//     <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-//     <title>Profile Generator</title>
-//     <style>
-//         @page {
-//           margin: 0;
-//         }
-//        *,
-//        *::after,
-//        *::before {
-//        box-sizing: border-box;
-//        }
-//        html, body {
-//        padding: 0;
-//        margin: 0;
-//        }
-//        html, body, .wrapper {
-//        height: 100%;
-//        }
-//        .wrapper {
-//        padding-top: 100px;
-//        }
-//        body {
-//        background-color: white;
-//        -webkit-print-color-adjust: exact !important;
-//        font-family: 'Cabin', sans-serif;
-//        }
-//        main {
-//        background-color: #E9EDEE;
-//        height: auto;
-//        padding-top: 30px;
-//        }
-//        h1, h2, h3, h4, h5, h6 {
-//        font-family: 'BioRhyme', serif;
-//        margin: 0;
-//        }
-//        h1 {
-//        font-size: 3em;
-//        }
-//        h2 {
-//        font-size: 2.5em;
-//        }
-//        h3 {
-//        font-size: 2em;
-//        }
-//        h4 {
-//        font-size: 1.5em;
-//        }
-//        h5 {
-//        font-size: 1.3em;
-//        }
-//        h6 {
-//        font-size: 1.2em;
-//        }
-//        .photo-header {
-//        position: relative;
-//        margin: 0 auto;
-//        margin-bottom: -50px;
-//        display: flex;
-//        justify-content: center;
-//        flex-wrap: wrap;
-//        padding: 10px;
-//        width: 95%;
-//        border-radius: 6px;
-//        }
-//        .photo-header img {
-//        width: 250px;
-//        height: 250px;
-//        border-radius: 50%;
-//        object-fit: cover;
-//        margin-top: -75px;
-//        border: 6px solid;
-//        box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-//        }
-//        .photo-header h1, .photo-header h2 {
-//        width: 100%;
-//        text-align: center;
-//        }
-//        .photo-header h1 {
-//        margin-top: 10px;
-//        }
-//        .links-nav {
-//        width: 100%;
-//        text-align: center;
-//        padding: 20px 0;
-//        font-size: 1.1em;
-//        }
-//        .nav-link {
-//        display: inline-block;
-//        margin: 5px 10px;
-//        }
-//        .workExp-date {
-//        font-style: italic;
-//        font-size: .7em;
-//        text-align: right;
-//        margin-top: 10px;
-//        }
-//        .container {
-//        padding: 50px;
-//        padding-left: 100px;
-//        padding-right: 100px;
-//        }
-
-//        .row {
-//          display: flex;
-//          flex-wrap: wrap;
-//          justify-content: space-between;
-//          margin-top: 20px;
-//          margin-bottom: 20px;
-//        }
-
-//        .card {
-//          padding: 20px;
-//          border-radius: 6px;
-//          margin: 20px;
-//        }
-       
-//        .col {
-//        flex: 1;
-//        text-align: center;
-//        }
-
-//        a, a:hover {
-//        text-decoration: none;
-//        color: inherit;
-//        font-weight: bold;
-//        }
-
-//        @media print { 
-//         body { 
-//           zoom: .75; 
-//         } 
-//        }
-//     </style>
-// </head>
-// <body>
-//     <header>
-//         <div class="wrapper">
-//         <img src="https://avatars1.githubusercontent.com/u/54192972?v=4" alt="Profile Image">
-//         </div>
-//         <h2>My name is Kelly Jones</h2>
-//         <h5>Currently @ Leidos ASC</h5>
-//         <h6>Location: <a href="https://www.gogle.com/maps/place/Denver">Denver, CO</a></h6>
-//         <h6>GitHub: <a href="https://github.com/kellyjones-jpg">kellyjones-jpg</a></h6>
-//         <h6>Blog: <a href="https://kellyjones-jpg.github.io/Professional-Materials">Professional-Materials</a>/</h6>
-//     </header>
-//     <main>
-//         <h2>I am a Web Editor for Leidos ASC, and am enrolled in the University of Denver’s 24-week Coding Boot Camp with an expected graduation date of March 14, 2020.</h2>
-//         <div class="card">
-//         <div class="card">
-//             <h3>Public Repositories</h3>
-//             <h4>18</h4>
-//         </div>
-
-//         <div class="card">
-//                 <h3>Followers</h3>
-//                 <h4>0</h4>
-//         </div>
-
-//         <div class="card">
-//                 <h3>GitHub Stars</h3>
-//                 <h4>0</h4>
-//         </div>
-
-//         <div class="card">
-//                 <h3>Following</h3>
-//                 <h4>0</h4>
-//         </div>
-
-//     </main>
-    
-// </body>
-// </html>`;
-// }
 
 promptUser()
   .then(function(answers) {
-    const html = generateHTML(answers);
 
-    return writeFileAsync("index.html", html);
+    mColor = answers.colorChoice;
+
+    gitData(answers);
+
   })
-  .then(function() {
-    console.log("Successfully wrote to index.html");
-  })
-  .catch(function(err) {
+ .catch(function(err) {
     console.log(err);
   });
