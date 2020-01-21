7d9639d6518e16c2021ec1c785a5205ff9f81f35
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-01-21 10:05:23 -0700

    making background changes

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function gitData(answers) {
   request(options, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       mData = JSON.parse(body);
+      mData.colorChoice = answers.colorChoice;
       mMapLocation = "http://www.google.com/maps/place/" + mData.location;
       const html = generateHTML(mData);
       // return writeFileAsync("index.html", html);
@@ -52,33 +53,6 @@ function gitData(answers) {
     }
   });
 }
-
-const colors = {
-  green: {
-    wrapperBackground: "#E6E1C3",
-    headerBackground: "#C1C72C",
-    headerColor: "black",
-    photoBorderColor: "#black"
-  },
-  blue: {
-    wrapperBackground: "#5F64D3",
-    headerBackground: "#26175A",
-    headerColor: "white",
-    photoBorderColor: "#73448C"
-  },
-  pink: {
-    wrapperBackground: "#879CDF",
-    headerBackground: "#FF8374",
-    headerColor: "white",
-    photoBorderColor: "#FEE24C"
-  },
-  red: {
-    wrapperBackground: "#DE9967",
-    headerBackground: "#870603",
-    headerColor: "white",
-    photoBorderColor: "white"
-  }
-};
 
 function generateHTML(userData) {
   return `
@@ -93,191 +67,118 @@ function generateHTML(userData) {
     <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap"
     rel="stylesheet">
     <title>Profile Generator</title>
-</head>
-<body>
-<div class="wrapper">
-    <header>
-        <div class="photo-header">
-        <div class="photo-header img">
-        <img src=${userData.avatar_url} alt="Profile Image" style="width:150px;height:150px;">
-        </div>
-        <h1>Hi!</h1>
-        <h2>My name is ${userData.login}!</h2>
-        <h5>Currently @ ${userData.company}</h5>
-        <p><a href=${mMapLocation}>${userData.location}</a></p>
-        <p><a href=${userData.html_url}>GitHub</a></p>
-        <p><a href=${userData.blog}>Blog</a></p>
-        </div>
-    </header>
-    <main>
-        <h3>${userData.bio}</h3>
-        <div class="card">
-            <h3>Public Repositories</h3>
-            <h4>${userData.public_repos}</h4>
-        </div>
-        <div class="card">
-                <h3>Followers</h3>
-                <h4>${userData.followers}</h4>
-        </div>
-        <div class="card">
-                <h3>GitHub Stars</h3>
-                <h4>${userData.public_gists}</h4>
-        </div>
-        <div class="card">
-                <h3>Following</h3>
-                <h4>${userData.following}</h4>
-        </div>
-    </main>
-    </div>
-</body>
-</html>
-<style>
-@page {
-  margin: 0;
-}
-*,
-*::after,
-*::before {
-box-sizing: border-box;
-}
-html, body {
-padding: 0;
-margin: 0;
-}
-html, body, .wrapper {
-height: 100%;
-}
-.wrapper {
-background-color: ${colors[data.color].wrapperBackground};
-padding-top: 100px;
-}
-body {
-background-color: white;
--webkit-print-color-adjust: exact !important;
-font-family: 'Cabin', sans-serif;
-}
-main {
-background-color: #E9EDEE;
-height: auto;
-padding-top: 30px;
-}
-h1, h2, h3, h4, h5, h6 {
-font-family: 'BioRhyme', serif;
-margin: 0;
-}
-h1 {
-font-size: 3em;
-}
-h2 {
-font-size: 2.5em;
-}
-h3 {
-font-size: 2em;
-}
-h4 {
-font-size: 1.5em;
-}
-h5 {
-font-size: 1.3em;
-}
-h6 {
-font-size: 1.2em;
-}
-.photo-header {
-position: relative;
-margin: 0 auto;
-margin-bottom: -50px;
-display: flex;
-justify-content: center;
-flex-wrap: wrap;
-background-color: ${colors[data.color].headerBackground};
-color: ${colors[data.color].headerColor};
-padding: 10px;
-width: 95%;
-border-radius: 6px;
-}
-.photo-header img {
-width: 250px;
-height: 250px;
-border-radius: 50%;
-object-fit: cover;
-margin-top: -75px;
-border: 6px solid ${colors[data.color].photoBorderColor};
-box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-}
-.photo-header h1, .photo-header h2 {
-width: 100%;
-text-align: center;
-}
-.photo-header h1 {
-margin-top: 10px;
-}
-.links-nav {
-width: 100%;
-text-align: center;
-padding: 20px 0;
-font-size: 1.1em;
-}
-.nav-link {
-display: inline-block;
-margin: 5px 10px;
-}
-.workExp-date {
-font-style: italic;
-font-size: .7em;
-text-align: right;
-margin-top: 10px;
-}
-.container {
-padding: 50px;
-padding-left: 100px;
-padding-right: 100px;
-}
-
-.row {
- display: flex;
- flex-wrap: wrap;
- justify-content: space-between;
- margin-top: 20px;
- margin-bottom: 20px;
-}
-
-.card {
- padding: 20px;
- border-radius: 6px;
- background-color: ${colors[data.color].headerBackground};
- color: ${colors[data.color].headerColor};
- margin: 20px;
-}
-
-.col {
-flex: 1;
-text-align: center;
-}
-
-a, a:hover {
-text-decoration: none;
-color: inherit;
-font-weight: bold;
-}
-
-@media print { 
-body { 
-  zoom: .75; 
-} 
-}
-</style>`;
-}
-
-
-promptUser()
-  .then(function (answers) {
-
-    mColor = answers.colorChoice;
-
-    gitData(answers);
-
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+    </head>
+    <style>
+      .green {
+          background-color: green;
+          color: white;
+          padding: 10px
+      }
+      .blue {
+          background-color: blue;
+          color: white;
+          padding: 10px;
+      }
+      .pink {
+          background-color: pink;
+          color: white;
+          padding: 10px
+      }
+      .red {
+          background-color: red;
+          color: white;
+          padding: 10px
+      }
+       h1 {text-align:center;}
+       h2 {
+          text-align:center;
+       }
+       h4 {text-align:center;}
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: ${userData.colorChoice};
+    }
+    li {
+      float: left;
+    }
+    li a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 16px;
+      text-decoration: none;
+    }
+    li a:hover {
+      background-color: #111111;
+    }
+    </style>
+    <body>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="wrapper" style="background-color:${userData.colorChoice};">
+       <div class="container">
+        <header>
+            <img src=${userData.avatar_url} class="rounded-circle mx-auto d-block" alt="Profile Image" style="width:150px;height:150px;">
+            <h1 class=${userData.colorChoice}>Hi!</h1>
+            <h2 class=${userData.colorChoice}>My name is ${userData.login}!</h2>
+            <h4 class=${userData.colorChoice}>Currently @ ${userData.company}</h4>
+            <ul>
+              <li><a href=${mMapLocation}>${userData.location}</a></li>
+              <li><a href=${userData.html_url}>GitHub</a></li>
+              <li><a href=${userData.blog}>Blog</a></li>
+            </ul>
+        </header>
+        <main>
+            </br>
+            <h3>${userData.bio}</h3>
+            </br>
+            <div class="container">
+              <div class="card-columns">
+                <div class="card" style="background-color:${userData.colorChoice};">
+                  <div class="card-body text-center">
+                    <h3 class=${userData.colorChoice}>Public Repositories</h3>
+                    <h4 class=${userData.colorChoice}>${userData.public_repos}</h4>
+                  </div>
+                </div>
+                <div class="card" style="background-color:${userData.colorChoice};">
+                  <div class="card-body text-center">
+                    <h3 class=${userData.colorChoice}>Followers</h3>
+                    <h4 class=${userData.colorChoice}>${userData.followers}</h4>
+                  </div>
+                </div>
+                <div class="card" style="background-color:${userData.colorChoice};">
+                  <div class="card-body text-center">
+                    <h3 class=${userData.colorChoice}>GitHub Stars</h3>
+                    <h4 class=${userData.colorChoice}>${userData.public_gists}</h4>
+                  </div>
+                </div>  
+                <div class="card" style="background-color:${userData.colorChoice};">
+                  <div class="card-body text-center">
+                    <h3 class=${userData.colorChoice}>Following</h3>
+                    <h4 class=${userData.colorChoice}>${userData.following}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </main>
+       </div>
+      </div>
+      </div>
+    </body>
+    </html>`;
+    }
+    
+    
+    promptUser()
+      .then(function(answers) {
+    
+        mColor = answers.colorChoice;
+    
+        gitData(answers);
+    
+      })
+     .catch(function(err) {
+        console.log(err);
+      });
